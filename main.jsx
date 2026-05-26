d115b5b19e707dbc7d2bb67e2b8b45cdf904e0e8
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-05-27 00:31:28 +0200

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EchoDungeon from './EchoDungeon.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <EchoDungeon />
+)
