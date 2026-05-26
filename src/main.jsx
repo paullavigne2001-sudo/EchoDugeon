7d0e894cf588aa54e7e2d5572fcccec9b5fe5f7e
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-05-27 00:53:02 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import EchoDungeon from '../EchoDungeon.jsx'
+import EchoDungeon from './EchoDungeon.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <EchoDungeon />
